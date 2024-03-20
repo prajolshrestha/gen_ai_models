76ea58e29a6ed60a1e1f897f0c01607fd8104f5f
Prajol Shrestha <prajolstha9@gmail.com>
2024-03-20 08:49:40 +0100

    testing my lamma finetuned model in hugging face

---
+++ LLM/LoRA/lamma_sample.py
@@ -0,0 +1,12 @@
+import torch
+from transformers import AutoTokenizer, LlamaForCausalLM
+
+model = LlamaForCausalLM.from_pretrained("prajolshrestha/llama-2-7b-finetuned")
+tokenizer = AutoTokenizer.from_pretrained("prajolshrestha/llama-2-7b-finetuned")
+
+prompt = "Hey, are you conscious? Can you talk to me?"
+inputs = tokenizer(prompt, return_tensors="pt")
+
+# Generate
+generate_ids = model.generate(inputs.input_ids, max_length=30)
+tokenizer.batch_decode(generate_ids, skip_special_tokens=True, clean_up_tokenization_spaces=False)[0]
